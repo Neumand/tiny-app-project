503ff816df62dbef1350a1bb2b9d6eef8d848c90
David <david.m.neuman@gmail.com>
2019-03-12 17:09:09 +0000

    Added views directory for EJS files. Added handler for GET requests on '/urls', which will keep track of all URLs and their shortened forms.

--- express_server.js
+++ express_server.js
@@ -20,6 +20,14 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// Used to keep track of all of the URLs and their shortened forms.
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
+});
+
 // Test functionality.
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
